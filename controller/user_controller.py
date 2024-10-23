2d1b308bfb70d8c23120305491b3709c2608a580
shorya1104 <pratapshorya123@gmail.com>
2024-10-23 21:38:48 +0530

    added single user fetch

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -2,16 +2,23 @@
 from app import app
 from model.user_model import user_model
 from flask import request, jsonify, abort
+from db_connect import db_connect
 obj=user_model()
+
 @app.route('/users')
 def get_all_users():
     return obj.fetch_all_users()
+
+@app.route('/users/<id>')
+def get_single_users(id):
+    return obj.fetch_single_user(id)
 
 @app.route("/user/adduser", methods=["POST"])
 def adduser():
     if not request.form or 'name' not in request.form or 'age' not in request.form or 'email' not in request.form:
             abort(400)  # Bad request if any field is missing
     return obj.add_new_users(request.form)
+
 @app.route("/users/delete/<id>", methods=["DELETE"])
 def delete_users(id):
     return obj.delete_user_model(id)
--- model/user_model.py
+++ model/user_model.py
@@ -1,17 +1,27 @@
 from flask import make_response
+from db_connect import db_connect
+import json
 class user_model():
     def __init__(self):
+        self.db=db_connect()
         self.users=[]
     def add_new_users(self,data):
-            self.users.append(data)
-            print(self.users)
-            return make_response({"message":"CREATED SUCCESSFULLY"})
+        print(data['name'])
+        self.db.cursor.execute(f"INSERT INTO users(name, email, phone, age, password, role) VALUES('{data['name']}', '{data['email']}', '{data['age']}', '{data['phone']}','{data['password']}', '{data['role']}')")
+        return make_response({"message":"CREATED SUCCESSFULLY"})
     def fetch_all_users(self):
-        if len(self.users)>0:
-            return self.users
-        else: return "NO DATA FOUND"
+         self.db.cursor.execute("SELECT * FROM users")
+         result=self.db.cursor.fetchall()
+         if len(result)>0:
+            return json.dumps(result)
+         else: return "NO DATA FOUND"
+    def fetch_single_user(self, id):
+         self.db.cursor.execute(f"SELECT * FROM users where id ={id}")
+         result=self.db.cursor.fetchall()
+         if len(result)>0:
+            return json.dumps(result)
+         else: return "NO DATA FOUND"
     def delete_user_model(self, id):
         print(id)
         self.users=[]
-        # self.users.pop(1)
         return ({"message":"User deleted successfully"})
