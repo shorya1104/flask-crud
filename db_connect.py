260a22fb5e57fd24fd52d8abfc68425be7fe2a15
shorya1104 <pratapshorya123@gmail.com>
2024-10-23 21:28:39 +0530

    added .env and db connection

---
+++ db_connect.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+class db_connect():
+    def __init__(self):
+        try:
+            self.con=mysql.connector.connect(host=os.getenv('DB_HOST'), user=os.getenv('DB_USER'),password=os.getenv('DB_PASSWORD'),database=os.getenv('DB_DATABASE'))
+            self.cursor = self.con.cursor(dictionary=True)
+            self.con.autocommit=True
+            print("Connection Succesfull")
+        except mysql.connector.Error as err:
+            print("Connection error",err)
+    def close(self):
+        if self.con:
+            self.cursor.close()
+            self.con.close()
+            print("Connection closed")
