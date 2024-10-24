cb47e3fd2670142e22d4795acffd920c115ac292
shorya1104 <pratapshorya123@gmail.com>
2024-10-24 13:31:17 +0530

    changes in code

--- model/user_model.py
+++ model/user_model.py
@@ -8,27 +8,27 @@ class user_model():
     def add_new_users(self,data):
         print(data['name'])
         self.db.cursor.execute(f"INSERT INTO users(name, email, phone, age, password, role) VALUES('{data['name']}', '{data['email']}', '{data['age']}', '{data['phone']}','{data['password']}', '{data['role']}')")
-        return make_response({"message":"CREATED SUCCESSFULLY"})
+        return make_response({"message":"CREATED SUCCESSFULLY"},201)
     def fetch_all_users(self):
          self.db.cursor.execute("SELECT * FROM users")
          result=self.db.cursor.fetchall()
          if len(result)>0:
-            return {"payload":result}
-         else: return "NO DATA FOUND"
+            return make_response({"payload":result},200)
+         else: return make_response({"message":"NO DATA FOUND"},204)
     def fetch_single_user(self, id):
          self.db.cursor.execute(f"SELECT * FROM users where id ={id}")
          result=self.db.cursor.fetchall()
          if len(result)>0:
-            return json.dumps(result)
-         else: return "NO DATA FOUND"
+            return make_response({"payload":result},200)
+         else: return make_response({"message":"NO DATA FOUND"},204)
     def update_user_mode(self, data):
         self.db.cursor.execute(f"UPDATE users SET name='{data['name']}', email='{data['email']}', phone='{data['phone']}', age='{data['age']}',password='{data['password']}',role='{data['role']}' WHERE id = {data['id']}")
         if self.db.cursor.rowcount>0:
-            return make_response({"message":"UPDATED SUCCESSFULLY"})
-        else: return make_response({"message":"NOTHING TO UPDATE"})
+            return make_response({"message":"UPDATED SUCCESSFULLY"},201)
+        else: return make_response({"message":"NOTHING TO UPDATE"},202)
     def delete_user_model(self, id):
         print(id)
         self.db.cursor.execute(f"DELETE FROM users WHERE id ={id}")
         if self.db.cursor.rowcount>0:
-            return ({"message":"User deleted successfully"})
-        else: return ({"message":"Nothing to delete"})
+            return make_response({"message":"User deleted successfully"},200)
+        else: return make_response({"message":"Nothing to delete"},202)
